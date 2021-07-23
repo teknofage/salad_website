fe3ac2a6c8d8d8932e2120976000b645472f5e51
Glitch (glitch-hello-node) <none>
2021-07-22 22:52:42 -0700

    fixed css on hero title

--- server.js
+++ server.js
@@ -46,21 +46,21 @@ fastify.get("/", function(request, reply) {
   // params is an object we'll pass to our handlebars template
   let params = { seo: seo };
   
-  // If someone clicked the option for a random color it'll be passed in the querystring
-  if (request.query.randomize) {
+  // // If someone clicked the option for a random color it'll be passed in the querystring
+  // if (request.query.randomize) {
     
-    // We need to load our color data file, pick one at random, and add it to the params
-    const colors = require("./src/colors.json");
-    const allColors = Object.keys(colors);
-    let currentColor = allColors[(allColors.length * Math.random()) << 0];
+  //   // We need to load our color data file, pick one at random, and add it to the params
+  //   const colors = require("./src/colors.json");
+  //   const allColors = Object.keys(colors);
+  //   let currentColor = allColors[(allColors.length * Math.random()) << 0];
     
-    // Add the color properties to the params object
-    params = {
-      color: colors[currentColor],
-      colorError: null,
-      seo: seo
-    };
-  }
+  //   // Add the color properties to the params object
+  //   params = {
+  //     color: colors[currentColor],
+  //     colorError: null,
+  //     seo: seo
+  //   };
+  // }
   
   // The Handlebars code will be able to access the parameter values and build them into the page
   reply.view("/src/pages/index.hbs", params);
@@ -76,37 +76,37 @@ fastify.post("/", function(request, reply) {
   // Build the params object to pass to the template
   let params = { seo: seo };
   
-  // If the user submitted a color through the form it'll be passed here in the request body
-  let color = request.body.color;
+  // // If the user submitted a color through the form it'll be passed here in the request body
+  // let color = request.body.color;
   
-  // If it's not empty, let's try to find the color
-  if (color) {
-    // ADD CODE FROM TODO HERE TO SAVE SUBMITTED FAVORITES
+  // // If it's not empty, let's try to find the color
+  // if (color) {
+  //   // ADD CODE FROM TODO HERE TO SAVE SUBMITTED FAVORITES
     
-    // Load our color data file
-    const colors = require("./src/colors.json");
+  //   // Load our color data file
+  //   const colors = require("./src/colors.json");
     
-    // Take our form submission, remove whitespace, and convert to lowercase
-    color = color.toLowerCase().replace(/\s/g, "");
+  //   // Take our form submission, remove whitespace, and convert to lowercase
+  //   color = color.toLowerCase().replace(/\s/g, "");
     
-    // Now we see if that color is a key in our colors object
-    if (colors[color]) {
+  //   // Now we see if that color is a key in our colors object
+  //   if (colors[color]) {
       
-      // Found one!
-      params = {
-        color: colors[color],
-        colorError: null,
-        seo: seo
-      };
-    } else {
+  //     // Found one!
+  //     params = {
+  //       color: colors[color],
+  //       colorError: null,
+  //       seo: seo
+  //     };
+  //   } else {
       
-      // No luck! Return the user value as the error property
-      params = {
-        colorError: request.body.color,
-        seo: seo
-      };
-    }
-  }
+  //     // No luck! Return the user value as the error property
+  //     params = {
+  //       colorError: request.body.color,
+  //       seo: seo
+  //     };
+  //   }
+  // }
   
   // The Handlebars template will use the parameter values to update the page with the chosen color
   reply.view("/src/pages/index.hbs", params);
