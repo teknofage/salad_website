4a394b229f9b2628b64ee86783278717ee1f5216
Glitch (glitch-hello-node) <none>
2021-07-23 00:07:38 -0700

    added working Book Now buttons, refactored views layout - moving towards partials - and embedded Calendly widget successfully

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
-/**
-* This is the main Node.js server script for your project
-* Check out the two endpoints this back-end API provides in fastify.get and fastify.post below
-*/
+const express = require('express');
+const app = express();
 
 const path = require("path");
+const handlebars = require('handlebars');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    // handlebars: allowInsecurePrototypeAccess(handlebars),
+});
+app.engine('handlebars', hbs.engine);
+// Use handlebars to render
+app.set('view engine', 'handlebars');
 
 // Require the fastify framework and instantiate it
 const fastify = require("fastify")({
   // Set this to true for detailed logging:
   logger: false
 });
-
-// ADD FAVORITES ARRAY VARIABLE FROM TODO HERE
 
 
 // Setup our static files
@@ -39,31 +44,14 @@ if (seo.url === "glitch-default") {
 /**
 * Our home page route
 *
-* Returns src/pages/index.hbs with data built into it
+* Returns src/pages/main.hbs with data built into it
 */
 fastify.get("/", function(request, reply) {
   
   // params is an object we'll pass to our handlebars template
   let params = { seo: seo };
   
-  // // If someone clicked the option for a random color it'll be passed in the querystring
-  // if (request.query.randomize) {
-    
-  //   // We need to load our color data file, pick one at random, and add it to the params
-  //   const colors = require("./src/colors.json");
-  //   const allColors = Object.keys(colors);
-  //   let currentColor = allColors[(allColors.length * Math.random()) << 0];
-    
-  //   // Add the color properties to the params object
-  //   params = {
-  //     color: colors[currentColor],
-  //     colorError: null,
-  //     seo: seo
-  //   };
-  // }
-  
-  // The Handlebars code will be able to access the parameter values and build them into the page
-  reply.view("/src/pages/index.hbs", params);
+  reply.view("/src/layouts/main.hbs", params);
 });
 
 /**
@@ -76,41 +64,39 @@ fastify.post("/", function(request, reply) {
   // Build the params object to pass to the template
   let params = { seo: seo };
   
-  // // If the user submitted a color through the form it'll be passed here in the request body
-  // let color = request.body.color;
+  // The Handlebars template will use the parameter values to update the page with the chosen color
+  reply.view("/src/layouts/main.hbs", params);
+});
+
+/**
+* Our book-now page route
+*
+* Returns src/pages/book-now.hbs with data built into it
+*/
+fastify.get("/book-now", function(request, reply) {
   
-  // // If it's not empty, let's try to find the color
-  // if (color) {
-  //   // ADD CODE FROM TODO HERE TO SAVE SUBMITTED FAVORITES
-    
-  //   // Load our color data file
-  //   const colors = require("./src/colors.json");
-    
-  //   // Take our form submission, remove whitespace, and convert to lowercase
-  //   color = color.toLowerCase().replace(/\s/g, "");
-    
-  //   // Now we see if that color is a key in our colors object
-  //   if (colors[color]) {
-      
-  //     // Found one!
-  //     params = {
-  //       color: colors[color],
-  //       colorError: null,
-  //       seo: seo
-  //     };
-  //   } else {
-      
-  //     // No luck! Return the user value as the error property
-  //     params = {
-  //       colorError: request.body.color,
-  //       seo: seo
-  //     };
-  //   }
-  // }
+  // params is an object we'll pass to our handlebars template
+  let params = { seo: seo };
+  
+  reply.view("/src/partials/book-now.hbs", params);
+});
+
+/**
+* Our POST route to handle and react to form submissions 
+*
+* Accepts body data indicating the user choice
+*/
+fastify.post("/book-now", function(request, reply) {
+  
+  // Build the params object to pass to the template
+  let params = { seo: seo };
   
   // The Handlebars template will use the parameter values to update the page with the chosen color
-  reply.view("/src/pages/index.hbs", params);
+  reply.view("/src/partials/book-now.hbs", params);
 });
+
+
+
 
 // Run the server and report out to the logs
 fastify.listen(process.env.PORT, function(err, address) {
